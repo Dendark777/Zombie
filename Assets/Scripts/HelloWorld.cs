5493cf5d8e1351840d956638141a98d92007e785
Tigon-444 <WTigon-444@Yandex.ru>
2022-09-11 16:46:21 +0300

    test поменял вывод

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello world Denis");
+        print("Hello world vet");
     }
 
     // Update is called once per frame
